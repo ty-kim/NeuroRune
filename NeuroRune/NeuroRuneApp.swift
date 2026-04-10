f0c575a2ea21c7974230b5832961c07a342fee2b
Taeyeon Kim <kimty8073@gmail.com>
2026-04-11 02:44:31 +0900

    chore: Xcode 프로젝트 생성

---
+++ NeuroRune/NeuroRuneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeuroRuneApp.swift
+//  NeuroRune
+//
+//  Created by tykim on 4/11/26.
+//
+
+import SwiftUI
+
+@main
+struct NeuroRuneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
